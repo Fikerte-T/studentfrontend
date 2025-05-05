8b51396d0ff92884b7cc4f2c8e1c611e83813746
fikerte <tofikertesfaye@gmail.com>
2025-05-05 14:55:46 +0300

    Add editing and deleting features

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -3,26 +3,72 @@ import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Container, Paper, Button} from "@mui/material"
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function Student() {
+    
     const paperSytle = {padding:"50px 20px", width:"600px", margin:"20px auto", display:"flex", flexDirection:"column", alignItems:"center"}
     const [name, setName] = useState('')
     const [address, setAddress] = useState('')
     const [students, setStudents] = useState([])
+    const [editingId, seteditingId] = useState(null);
+
     const handleSubmit = e => {
         e.preventDefault()
         const student = {name, address}
-        fetch('http://localhost:8080/student/add', {
-            method: "POST",
+        if(editingId === null) {
+            fetch('http://localhost:8080/student/add', {
+                method: "POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(student)
+            })
+            .then(() => {
+                setName('')
+                setAddress('')
+            })
+        } else {
+            fetch(`http://localhost:8080/student/edit/${editingId}`, {
+            method: "PUT",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify(student)
-        }).then(() => console.log("new student added"))
+        })
+        .then(() => {
+            setName('')
+            setAddress('')
+            seteditingId(null)
+        })
+        }
     }
+    const handleDelete = id => {
+        fetch('http://localhost:8080/student/' + id, {
+            method: 'DELETE',
+          })
+          .then(res => {
+            if(res.ok) {
+                setStudents(students.filter(student => student.id !== id))
+            } else {
+                console.log("error deleting student")
+            }
+          })
+          .catch(error => {
+            console.error("error deleting student:", error)
+          })
+        }
+
+    const handleUpdate = id => {
+        const student = students.find(s => s.id === id)
+        console.log(student)
+        setName(student.name)
+        setAddress(student.address)
+        seteditingId(id)
+    }
+
     useEffect(()=> {
         fetch('http://localhost:8080/student/getAll')
         .then(resp => resp.json())
         .then(result => setStudents(result))
-    }, [])
+    }, [students])
  return (
     
     <Container>
@@ -43,7 +89,7 @@ export default function Student() {
             onChange={e => setAddress(e.target.value)}
             />
         </Box>
-        <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+        <Button variant="contained" onClick={handleSubmit}>{editingId === null ? "Submit" : "Update" }</Button>
         </Paper>
         <Paper elevation={3} style={paperSytle}>
             <h1>Students</h1>
@@ -52,6 +98,12 @@ export default function Student() {
                     id: {student.id} <br/>
                     Name: {student.name} <br/>
                     Address: {student.address}
+                    <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => handleDelete(student.id)}>
+                        Delete
+                    </Button>
+                    <Button variant="outlined" startIcon={<EditIcon />} onClick={() => handleUpdate(student.id)} >
+                        Update
+                    </Button>
                 </Paper>
             ))}
         </Paper>
